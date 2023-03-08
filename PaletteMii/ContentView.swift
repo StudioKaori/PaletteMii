24324c4e3a88b659949d6d0d7a90de2af3bf044c
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-03-08 15:29:05 +0100

    Add registerSample data function

--- PaletteMii/ContentView.swift
+++ PaletteMii/ContentView.swift
@@ -9,80 +9,89 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-      sortDescriptors: [NSSortDescriptor(keyPath: \Palette.id, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Palette>
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.title!)")
-                    } label: {
-                        Text(item.title!)
-                    }
-                }
-                .onDelete(perform: deleteItems)
+  @Environment(\.managedObjectContext) private var viewContext
+  
+  @FetchRequest(
+    sortDescriptors: [NSSortDescriptor(keyPath: \Palette.id, ascending: true)],
+    animation: .default)
+  private var items: FetchedResults<Palette>
+  
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(items) { item in
+          NavigationLink {
+            HStack {
+              Text("Item at \(item.title!)")
+              
+              ForEach(PersistenceController.shared.toColorHexArray(item.colorHexes), id: \.self) { color in
+                Text(color.hex!)
+              }
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+          } label: {
+            Text(item.title!)
+          }
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Palette(context: viewContext)
-            newItem.title = "Added sample"
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+        .onDelete(perform: deleteItems)
+      }
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          EditButton()
         }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+        ToolbarItem {
+          Button(action: addItem) {
+            Label("Add Item", systemImage: "plus")
+          }
         }
+      }
+      Text("Select an item")
     }
+    .onAppear {
+      PersistenceController.shared.registSampleData(viewContext: viewContext)
+    }
+  }
+  
+  private func addItem() {
+    withAnimation {
+      let newItem = Palette(context: viewContext)
+      newItem.title = "Added sample"
+      
+      do {
+        try viewContext.save()
+      } catch {
+        // Replace this implementation with code to handle the error appropriately.
+        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        let nsError = error as NSError
+        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+      }
+    }
+  }
+  
+  private func deleteItems(offsets: IndexSet) {
+    withAnimation {
+      offsets.map { items[$0] }.forEach(viewContext.delete)
+      
+      do {
+        try viewContext.save()
+      } catch {
+        // Replace this implementation with code to handle the error appropriately.
+        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        let nsError = error as NSError
+        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+      }
+    }
+  }
 }
 
 private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
+  let formatter = DateFormatter()
+  formatter.dateStyle = .short
+  formatter.timeStyle = .medium
+  return formatter
 }()
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
+  static var previews: some View {
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+  }
 }
--- PaletteMii/Persistence.swift
+++ PaletteMii/Persistence.swift
@@ -8,50 +8,163 @@
 import CoreData
 
 struct PersistenceController {
-    static let shared = PersistenceController()
+  static let shared = PersistenceController()
+  
+  static var preview: PersistenceController = {
+    let result = PersistenceController(inMemory: true)
+    let viewContext = result.container.viewContext
+    
+    // Sample data
+    //
+    //    for _ in 0..<10 {
+    //      let newItem = Palette(context: viewContext)
+    //      newItem.id = UUID().uuidString
+    //      newItem.title = "Sample"
+    //    }
 
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Palette(context: viewContext)
-          newItem.id = UUID().uuidString
-          newItem.title = "Sample"
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
-
-    let container: NSPersistentContainer
-
-    init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "PaletteMiiContainer")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        container.viewContext.automaticallyMergesChangesFromParent = true
+    /// 新しいpaletteList
+    let paletteLists: [(String, [String])] = [
+      ("my palette1", ["93AEC1", "FF0000", "00FF00"]),
+      ("my palette2", ["9DBDBA", "00FFFF", "FF0000"]),
+      ("my palette3", ["F8B042", "FF00FF", "00FFFF"])
+    ]
+    /// Paletteテーブル全消去
+    let fetchRequestPalette = NSFetchRequest<NSFetchRequestResult>(entityName: "Palette")
+    fetchRequestPalette.entity = Palette.entity()
+    let palettes = try? viewContext.fetch(fetchRequestPalette) as? [Palette]
+    for palette in palettes! {
+      viewContext.delete(palette)
     }
+
+    /// Paletteテーブル登録
+    for (title, colorHexList) in paletteLists {
+      let newPalette = Palette(context: viewContext)
+      newPalette.id = UUID().uuidString
+      newPalette.title = title
+
+      // register colorHexes
+      for colorHex in colorHexList {
+        let fetchRequestColorHex = NSFetchRequest<NSFetchRequestResult>(entityName: "ColorHex")
+        fetchRequestColorHex.entity = ColorHex.entity()
+        fetchRequestColorHex.predicate = NSPredicate(format: "hex = %@", colorHex)
+        let result = try? viewContext.fetch(fetchRequestColorHex) as? [ColorHex]
+        if let existingColorHex = result?.first {
+          /// 既に存在している場合はそれを使用
+          newPalette.addToColorHexes(existingColorHex)
+        } else {
+          /// 存在しない場合は新規に作成
+          let newColorHex = ColorHex(context: viewContext)
+          newColorHex.hex = colorHex
+          newPalette.addToColorHexes(newColorHex)
+        }
+      } // END: colorHex
+      
+    }
+
+    do {
+      try viewContext.save()
+    } catch {
+      // Replace this implementation with code to handle the error appropriately.
+      // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+      let nsError = error as NSError
+      fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+    }
+    return result
+  }()
+  
+  let container: NSPersistentContainer
+  
+  init(inMemory: Bool = false) {
+    container = NSPersistentContainer(name: "PaletteMiiContainer")
+    if inMemory {
+      container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+    }
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+      if let error = error as NSError? {
+        // Replace this implementation with code to handle the error appropriately.
+        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        
+        /*
+         Typical reasons for an error here include:
+         * The parent directory does not exist, cannot be created, or disallows writing.
+         * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+         * The device is out of space.
+         * The store could not be migrated to the current model version.
+         Check the error message to determine what the actual problem was.
+         */
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    })
+    container.viewContext.automaticallyMergesChangesFromParent = true
+  }
+  
+  
+  func registSampleData(viewContext: NSManagedObjectContext) {
+    
+    /// 新しいpaletteList
+    let paletteLists: [(String, [String])] = [
+      ("my palette1", ["93AEC1", "FF0000", "00FF00"]),
+      ("my palette2", ["9DBDBA", "00FFFF", "FF0000"]),
+      ("my palette3", ["F8B042", "FF00FF", "00FFFF"])
+    ]
+    /// Paletteテーブル全消去
+    let fetchRequestPalette = NSFetchRequest<NSFetchRequestResult>(entityName: "Palette")
+    fetchRequestPalette.entity = Palette.entity()
+    let palettes = try? viewContext.fetch(fetchRequestPalette) as? [Palette]
+    for palette in palettes! {
+      viewContext.delete(palette)
+    }
+
+    /// Paletteテーブル登録
+    for (title, colorHexList) in paletteLists {
+      let newPalette = Palette(context: viewContext)
+      newPalette.id = UUID().uuidString
+      newPalette.title = title
+
+      // register colorHexes
+      for colorHex in colorHexList {
+        let fetchRequestColorHex = NSFetchRequest<NSFetchRequestResult>(entityName: "ColorHex")
+        fetchRequestColorHex.entity = ColorHex.entity()
+        fetchRequestColorHex.predicate = NSPredicate(format: "hex = %@", colorHex)
+        let result = try? viewContext.fetch(fetchRequestColorHex) as? [ColorHex]
+        if let existingColorHex = result?.first {
+          /// 既に存在している場合はそれを使用
+          newPalette.addToColorHexes(existingColorHex)
+        } else {
+          /// 存在しない場合は新規に作成
+          let newColorHex = ColorHex(context: viewContext)
+          newColorHex.hex = colorHex
+          newPalette.addToColorHexes(newColorHex)
+        }
+      } // END: colorHex
+      
+    }
+
+
+    
+  //
+  //    for _ in 0..<10 {
+  //      let newItem = Palette(context: viewContext)
+  //      newItem.id = UUID().uuidString
+  //      newItem.title = "Sample"
+  //    }
+    do {
+      try viewContext.save()
+    } catch {
+      // Replace this implementation with code to handle the error appropriately.
+      // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+      let nsError = error as NSError
+      fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+    }
+  }
+
+  
+  /// NSSet? → [ColorHex]変換
+  func toColorHexArray(_ colorHexes: NSSet?) -> [ColorHex] {
+      guard let colorHexes = colorHexes as? Set<ColorHex> else {
+          return []
+      }
+      return Array(colorHexes)
+  }
+
 }
