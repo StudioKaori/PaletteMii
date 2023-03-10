f875ced504c00872c5dc11d50c3a763a6063eb50
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-03-10 13:37:40 +0100

    Add dataManager

---
+++ PaletteMii/Utilities/PaletteDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  PaletteMii
+//
+//  Created by Kaori Persson on 2023-03-10.
+//
+
+import SwiftUI
+
+class PaletteDataManager: ObservableObject {
+  @Published var palettes: [Palette] = []
+
+}
